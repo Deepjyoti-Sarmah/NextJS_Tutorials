594a9ad6f703691299f53fca417f767dda5b92d2
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-09 13:32:26 +0530

    adding database model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -15,7 +15,18 @@ const userSchema = new mongoose.Schema({
         trype: String,
         required: [true, "Please provide a password"],
     },
-    
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
 })
 
 const User = mongoose.models.users || mongoose.model("user", userSchema);
