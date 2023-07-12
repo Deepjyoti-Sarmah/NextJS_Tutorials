bb7ecbe1f35f9ee583c8524aa9a05a1013bee3ee
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-12 16:40:19 +0530

    signup and login done

--- auth_next/src/app/signup/page.tsx
+++ auth_next/src/app/signup/page.tsx
@@ -7,23 +7,20 @@ import { toast } from "react-hot-toast";
 
 export default function SignupPage() {
     const router = useRouter();
-
     const [user, setUser] = React.useState({
         email: "",
         password: "",
-        userName: "",
+        username: "",
     });
-
-    const [buttonDisabled, setButtonDiabled] = React.useState(false);
+    
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
-
-    const onSignUp = async () => {
+    const onSignup = async () => {
         try {
             setLoading(true);
             const response = await axios.post("/api/users/signup", user);
             console.log("Signup success", response.data);
-
             router.push("/login");
 
         } catch (error: any) {
@@ -33,15 +30,16 @@ export default function SignupPage() {
         } finally {
             setLoading(false);
         }
-    };
+    }
 
     useEffect(() => {
-        if (user.email.length > 0 && user.password.length > 0 && user.userName.length > 0) {
-            setButtonDiabled(false);
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
         } else {
-            setButtonDiabled(true);
+            setButtonDisabled(true);
         }
     }, [user]);
+
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -49,51 +47,35 @@ export default function SignupPage() {
             <hr />
             <label htmlFor="username">username</label>
             <input
-                className="p-2 border border-gray-300       
-                    rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="username"
                 type="text"
-                value={user.userName}
-                onChange={(e) => setUser({ ...user, userName: e.target.value })}
+                value={user.username}
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
                 placeholder="username"
             />
             <label htmlFor="email">email</label>
             <input
-                className="p-2 border border-gray-300 
-                    rounded-lg mb-4 focus:outline-none 
-                    focus:border-gray-600 text-black"
-                id="username"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                id="email"
                 type="text"
                 value={user.email}
-                onChange={(e) =>
-                    setUser({
-                        ...user,
-                        email: e.target.value,
-                    })
-                }
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
                 placeholder="email"
             />
             <label htmlFor="password">password</label>
             <input
-                className="p-2 border border-gray-300 
-                    rounded-lg mb-4 focus:outline-none 
-                    focus:border-gray-600 text-black"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="password"
                 type="password"
                 value={user.password}
-                onChange={(e) =>
-                    setUser({
-                        ...user,
-                        password: e.target.value,
-                    })
-                }
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
                 placeholder="password"
             />
             <button
-                onClick={onSignUp}
+                onClick={onSignup}
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
             <Link href="/login">Visit login page</Link>
         </div>
-    );
+    )
 }
-// hritesh0141@gmail.com
