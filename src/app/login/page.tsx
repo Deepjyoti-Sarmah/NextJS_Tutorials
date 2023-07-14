8100097106b4e646d4993ea137dd3a2bfdcd7db3
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-14 21:49:18 +0530

    error removed now signup and login working

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,26 @@
 "use client";
 import Link from "next/link";
-import React, { use, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
     const router = useRouter();
-
     const [user, setUser] = React.useState({
         email: "",
         password: "",
-    });
-
+    })
     const [buttonDisabled, setButtonDisabled] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
+
 
     const onLogin = async () => {
         try {
             setLoading(true);
             const response = await axios.post("/api/users/login", user);
-            console.log("login success", response.data);
-            toast.success("login success");
+            console.log("Login success", response.data);
+            toast.success("Login success");
             router.push("/profile");
         } catch (error: any) {
             console.log("Login failed", error.message);
@@ -29,7 +28,7 @@ export default function LoginPage() {
         } finally {
             setLoading(false);
         }
-    };
+    }
 
     useEffect(() => {
         if (user.email.length > 0 && user.password.length > 0) {
@@ -43,36 +42,23 @@ export default function LoginPage() {
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>{loading ? "Processing" : "Login"}</h1>
             <hr />
+
             <label htmlFor="email">email</label>
             <input
-                className="p-2 border border-gray-300 
-                    rounded-lg mb-4 focus:outline-none 
-                    focus:border-gray-600 text-black"
-                id="username"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+                id="email"
                 type="text"
                 value={user.email}
-                onChange={(e) =>
-                    setUser({
-                        ...user,
-                        email: e.target.value,
-                    })
-                }
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
                 placeholder="email"
             />
             <label htmlFor="password">password</label>
             <input
-                className="p-2 border border-gray-300 
-                    rounded-lg mb-4 focus:outline-none 
-                    focus:border-gray-600 text-black"
+                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
                 id="password"
                 type="password"
                 value={user.password}
-                onChange={(e) =>
-                    setUser({
-                        ...user,
-                        password: e.target.value,
-                    })
-                }
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
                 placeholder="password"
             />
             <button
@@ -80,5 +66,6 @@ export default function LoginPage() {
                 className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
             <Link href="/signup">Visit Signup page</Link>
         </div>
-    );
+    )
+
 }
