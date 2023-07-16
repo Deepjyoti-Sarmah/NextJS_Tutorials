b78e1dded201ab96be5ff9d1ec74335b67cff1d8
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-07-16 16:14:41 +0530

    error removed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,22 @@
 import { NextResponse } from 'next/server'
-import { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname
 
-    const isPublicPath = path === '/login' || path === '/signup';
+    const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail'
 
-    const token = request.cookies.get('token')?.value || '';
+    const token = request.cookies.get('token')?.value || ''
 
     if(isPublicPath && token) {
-        return NextResponse.redirect(new URL('/', request.nextUrl));
+        return NextResponse.redirect(new URL('/', request.nextUrl))
     }
 
-    if(!isPublicPath && !token) {
-        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
     }
-
-
+    
 }
 
 // See "Matching Paths" below to learn more
@@ -27,5 +26,6 @@ export const config = {
         '/profile',
         '/login',
         '/signup',
+        // '/verifyemail'
     ]
 }
